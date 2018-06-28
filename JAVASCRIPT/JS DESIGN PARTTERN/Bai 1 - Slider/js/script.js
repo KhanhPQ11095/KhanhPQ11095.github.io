bdc5f782cbe761e6ef1564340cf8c600a2f7e1e0
KhanhPQ11095 <pham-khanh@hotmail.com>
2018-06-28 16:23:37 +0700

    JAVASCRIPT DESIGN PATTERN

--- JAVASCRIPT/JS DESIGN PARTTERN/Bai 1 - Slider/js/script.js
+++ JAVASCRIPT/JS DESIGN PARTTERN/Bai 1 - Slider/js/script.js
@@ -1,41 +1,124 @@
-window.onload = function () {
+$(document).ready(function() {
 	var slideIndex = 1;
-	var array = document.querySelectorAll('.slider img');
-	var image = document.querySelectorAll('.slide-mini img');
-	var firstImage = true;
 
-	var i;
+	// create mySlideModule
+	var mySlideModule = ( function() {	
 
+	//config
+	var width = 800;
+	var speedAnimation = 1000;
+	var pause = 3000;
+
+	//cache DOM
+	var $slider = $('#slider');
+	var $slidesContainer = $slider.find('.slides-container');
+	var $slide = $slidesContainer.find('.slide');
+	var interval;
+	
+	//show slider 
 	function slideShow(n) {
-
-		if (n > array.length) {
-			slideIndex = 1;
+		slideIndex = n;
+		
+		//init for slider
+		$("#box"+(slideIndex)).addClass('active');
+		var location = width * slideIndex; 
+		$(".slide").css("opacity",0.3);
+		$(".slide").eq(slideIndex-1).css("opacity",1); 
+		$slidesContainer.animate({'margin-left' :'-'+(location - width)+'px'}, speedAnimation/2);
+		
+		//set slider run automatic
+			interval = setInterval(function() {
+				$(".slide").css("opacity",1);
+				$(".box").removeClass('active');
+				$("#box"+(slideIndex+1)).addClass('active');
+				location = width *(slideIndex); 
+				$slidesContainer.animate({'margin-left' :'-'+location+'px'}, speedAnimation, function() {
+					slideIndex++;			   
+					if(slideIndex == $slide.length) {
+						$("#box1").addClass('active');
+						slideIndex = 1;
+						$slidesContainer.css('margin-left', '0px');
+					}
+				});
+				$( ".slide" ).eq(slideIndex-1).css("opacity",0.3);
+			},pause);
 		}
-
-		if (n < 1) {
-			slideIndex = array.length;
-		}
-
-		for (i = 0; i < array.length; i++) {
-			array[i].style.display = "none";
-		}
-
-		for (i = 0; i < image.length; i++) {
-			image[i].className = image[i].className.replace(" active", "")
-		}
-
-		array[slideIndex-1].style.display = "block";
-		image[slideIndex-1].className += " active";
+	
+	//stop Slide
+	function slideStop() {
+		clearInterval(interval);
+		$(".box").removeClass('active');
 	}
-
-	function dir(n) {
-		slideShow(slideIndex+=n);
+	
+	var timeClick = 0;
+	
+	//click previous button
+	function slidePrevious () {
+		var date = new Date;
+		var time = date.getSeconds();
+		if (time != timeClick) {
+			timeClick = time;
+			slideStop();
+			slideIndex--;
+			if(slideIndex < 1) { 
+				slideIndex = 4;
+			}
+			slideShow(slideIndex);
+		}
 	}
-
-	function box(n) {
-		slideShow(slideIndex=n);
+	
+	//click next button
+	function slideNext() {
+		var date = new Date;
+		var time = date.getSeconds();
+		if (time != timeClick) {
+			timeClick = time;
+			slideStop()
+			slideIndex++;
+			if(slideIndex > 4) {
+				slideIndex = 1;
+			}
+			slideShow(slideIndex);
+		}
 	}
-
-	slideShow(slideIndex);
-	setInterval(function() { dir(1) }, 3000);
-}
+	
+	//set when click
+	function slideClick(n) {
+		var date = new Date;
+		var time = date.getSeconds();
+		if (time != timeClick) {
+			timeClick = time;
+			slideStop();
+			slideIndex = n;
+			slideShow(slideIndex); 
+		}		
+	};
+	
+	function slideInt() {
+		slideShow(slideIndex);
+	};
+	
+	return {
+		show: slideInt ,
+		previous: slidePrevious ,
+		next: slideNext ,
+		click: slideClick
+	}
+	
+	})();
+	
+	//call mySlideModule
+	mySlideModule.show();
+	$("#prev").click(function () {
+		mySlideModule.previous();
+	});
+	
+	$("#next").click(function () {
+		mySlideModule.next();
+	});
+	
+	jQuery.fn.onClick = function(n) {
+		mySlideModule.click(n);
+	};
+	
+});
